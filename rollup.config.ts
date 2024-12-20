759ca1e1541b07d5a628fdf88a3245d609d1c2e1
Ilya <iosdsbb@gmail.com>
2024-12-21 01:12:18 +0300

    fix: path for rollup

--- rollup.config.ts
+++ rollup.config.ts
@@ -7,7 +7,7 @@ import del from "rollup-plugin-delete";
 import pkg from "./package.json" with {type: "json"};
 
 const config = {
-	input: "src/lib.ts",
+	input: "src/lib/index.ts",
 	output: [
 		{
 			file: pkg.main,
@@ -43,9 +43,10 @@ const config = {
 		}),
 		alias({
 			entries: [
-				{find: '~core', replacement: 'src/core'},
-				{find: '~assets', replacement: 'assets'},
-			]
+				{ find: "~lib", replacement: "src/lib" },
+				{ find: "~core", replacement: "src/lib/core" },
+				{ find: "~assets", replacement: "assets" },
+			],
 		}),
 	],
 };
